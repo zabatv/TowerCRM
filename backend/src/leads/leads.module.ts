b677395c1b5088673c5ea41251536a9a60a8e953
zabatv <germanpetrov09082009@gmail.com>
2026-05-29 22:56:12 +0300

    Import NotificationsModule in LeadsModule

--- backend/src/leads/leads.module.ts
+++ backend/src/leads/leads.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { LeadsController } from './leads.controller';
 import { LeadsService } from './leads.service';
+import { NotificationsModule } from '../notifications/notifications.module';
 
 @Module({
+  imports: [NotificationsModule],
   controllers: [LeadsController],
   providers: [LeadsService],
   exports: [LeadsService],
